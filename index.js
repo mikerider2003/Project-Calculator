3d16f9cc22cfac2ddb127a91c1a161c2675e2e2f
mikerider2003 <90722390+mikerider2003@users.noreply.github.com>
2023-07-23 19:25:05 +0200

    Final functionality update

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-var firstVariable;
-var secondVariable;
-var operator;
-
-
 function add(number1, number2){
     return number1 + number2;
 };
@@ -12,7 +7,7 @@ function subtract(number1, number2){
 };
 
 function multiply(number1, number2) {
-    return number1 * number2;
+    return (number1 * number2).toFixed(2);
 }
 
 function devide(number1, number2) {
@@ -20,7 +15,7 @@ function devide(number1, number2) {
         return "ERROR cannot devide zero !";
     };
 
-    return number1 / number2;
+    return (number1 / number2).toFixed(2);
 }
 
 
@@ -47,25 +42,57 @@ const buttons = document.querySelectorAll("button");
 const screen = document.querySelector(".screen");
 const screenChildren = screen.querySelectorAll("div");
 
+var firstVariable;
+var secondVariable;
+var operator;
+
 buttons.forEach(button => {
     button.addEventListener("click", () => {
 
         const pressedButton = button.textContent;
-        console.log(pressedButton);
 
         if ("0123456789".includes(pressedButton)){
+            if (operator !== undefined && secondVariable === undefined){
+                screenChildren[1].textContent = "";
+                secondVariable = 0;
+            };
             screenChildren[1].textContent += pressedButton;
         };
-
+        
         if ("+-*/".includes(pressedButton)) {
-            screenChildren[0].textContent = screenChildren[1].textContent + pressedButton;
-            screenChildren[1].textContent = "";
+            firstVariable = screenChildren[1].textContent;
+            if (firstVariable == ""){
+                firstVariable = 0;
+            };
+            operator = pressedButton;
+            screenChildren[0].textContent = firstVariable + operator;
+        };
+
+        if ("=".includes(pressedButton)){
+            secondVariable = screenChildren[1].textContent;
+            screenChildren[0].textContent += secondVariable + pressedButton;
+
+            let result = operate(Number(firstVariable), Number(secondVariable), operator);
+
+            screenChildren[1].textContent = result;
+
+            console.log(firstVariable);
+            console.log(operator);
+            console.log(secondVariable);
+
+            firstVariable = undefined;
+            secondVariable = undefined;
+            operator = undefined;
         };
 
         if ("C".includes(pressedButton)){
             screenChildren[0].textContent = "";
             screenChildren[1].textContent = "";
+            
+            firstVariable = undefined;
+            secondVariable = undefined;
+            operator = undefined;
         };
-
+        
     });
 });
